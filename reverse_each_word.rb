507d2bb39960c76449a57085f8b47b05be3ec68f
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 22:11:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   reversed = ""
   array = sentence.split
   array.each do |word|
-    reversed << "word.reverse "
+    reversed << "#{word.reverse} "
   end
   reversed
 end
