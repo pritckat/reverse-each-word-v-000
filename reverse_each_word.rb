9c56b2f29b14ada73b4385b5995879326677efcb
Katrina Pritchard <pritckat@gmail.com>
2018-08-14 22:15:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(sentence)
   array = sentence.split
   array.collect do |word|
     word.reverse
-    array.join(" ")
     #reversed << "#{word.reverse}"
   end
+  array.join(" ")
   #reversed.join(" ")
 end
